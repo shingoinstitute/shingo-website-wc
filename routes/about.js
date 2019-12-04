cfdb78a7deecb2027ba70759fe3ad28f520aa768
Justin Bresee <justinbresee@gmail.com>
2019-12-03 17:20:24 -0700

    created a component for the shingo staff so that the display on the website is dynamic

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const jsforce = require('jsforce');
-const fecha = require('fecha');
 const conn = new jsforce.Connection();
 
 conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD + process.env.SF_SECURITY_TOKEN, function(err, res) {
@@ -23,49 +22,63 @@ router.get('/academy', function(request, response, next) {
     });
   });
   
-  /* GET Shingo Alumni Members Page. */
-  router.get('/alumni', function(request, response, next) {
-  
-    //Query SalesForce
-    const query = 'Select Id, Name, Account.Name, Title From Contact Where Shingo_Prize_Relationship__c Includes (\'Alumni\') ORDER BY LastName'
-    conn.query(query, function(err, res) {
-      if (err) { return console.error(err); }
-  
-      // RENDER VIEW
-      response.render('About/shingoAlumni', 
-      { title: 'Shingo Alumni Members',
-        alumni: res.records});
-    });
-  });
-  
-  /* GET Shingo Site Examiners Page. */
-  router.get('/site-examiners', function(request, response, next) {
-  
-    //Query SalesForce
-    const query = 'SELECT Id, Name, Title, Account.Name FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES(\'Site Examiner\') AND Account.Name != null ORDER BY LastName'
-    conn.query(query, function(err, res) {
-      if (err) { return console.error(err); }
-  
-      // RENDER VIEW
-      response.render('About/siteExaminers', 
-      { title: 'Shingo Site Examiners',
-        examiners: res.records});
-    });
-  });
-  
-  /* GET Shingo Reseach & Publication Examiners Page. */
-  router.get('/research-examiners', function(request, response, next) {
-  
-    //Query SalesForce
-    const query = 'SELECT Id, Name, Title, Account.Name FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES(\'Research Examiner\',\'Publication Examiner\') ORDER BY LastName'
-    conn.query(query, function(err, res) {
-      if (err) { return console.error(err); }
-  
-      // RENDER VIEW
-      response.render('About/researchAndPublicationExaminers', 
-      { title: 'Shingo Research & Publication Examiners',
-        examiners: res.records});
-    });
-  });
+/* GET Shingo Alumni Members Page. */
+router.get('/alumni', function(request, response, next) {
 
-  module.exports = router;
+  //Query SalesForce
+  const query = 'Select Id, Name, Account.Name, Title From Contact Where Shingo_Prize_Relationship__c Includes (\'Alumni\') ORDER BY LastName'
+  conn.query(query, function(err, res) {
+    if (err) { return console.error(err); }
+
+    // RENDER VIEW
+    response.render('About/shingoAlumni', 
+    { title: 'Shingo Alumni Members',
+      alumni: res.records});
+  });
+});
+  
+/* GET Shingo Site Examiners Page. */
+router.get('/site-examiners', function(request, response, next) {
+
+  //Query SalesForce
+  const query = 'SELECT Id, Name, Title, Account.Name FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES(\'Site Examiner\') AND Account.Name != null ORDER BY LastName'
+  conn.query(query, function(err, res) {
+    if (err) { return console.error(err); }
+
+    // RENDER VIEW
+    response.render('About/siteExaminers', 
+    { title: 'Shingo Site Examiners',
+      examiners: res.records});
+  });
+});
+
+/* GET Shingo Reseach & Publication Examiners Page. */
+router.get('/research-examiners', function(request, response, next) {
+
+  //Query SalesForce
+  const query = 'SELECT Id, Name, Title, Account.Name FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES(\'Research Examiner\',\'Publication Examiner\') ORDER BY LastName'
+  conn.query(query, function(err, res) {
+    if (err) { return console.error(err); }
+
+    // RENDER VIEW
+    response.render('About/researchAndPublicationExaminers', 
+    { title: 'Shingo Research & Publication Examiners',
+      examiners: res.records});
+  });
+});
+
+router.get('/staff', function(request, response, next) {
+
+  //Query SalesForce
+  const query = "SELECT Name, Title, Email, Phone, Photograph__c,Account.Name  FROM Contact WHERE Account.Name = 'Shingo Institute' ORDER BY LastName";
+  conn.query(query, function(err, res) {
+    if (err) { return console.error(err); }
+
+    // RENDER VIEW
+    response.render('About/staff', 
+    { title: 'Shingo Research & Publication Examiners',
+      employees: res.records});
+  });
+});
+
+module.exports = router;
