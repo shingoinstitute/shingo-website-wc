f83cf73c3ef8795d2ea7510b8a610f2ada571988
Justin Bresee <justinbresee@gmail.com>
2020-01-23 11:32:12 -0700

    changed the interval at which the app pings itself to keep the app alive so that it is less frequent and doesn't use quite as much bandwidth

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,70 @@
 // NPM Module Imports
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const chalk = require('chalk');
+const chalk = require("chalk");
 const http = require("http");
 
-
 // CONNECT to salesforce & Set up PORT
-const jsforce = require('jsforce');
+const jsforce = require("jsforce");
 const conn = new jsforce.Connection();
 
 // LOGIN TO SALESFORCE
-conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD + process.env.SF_SECURITY_TOKEN, function(err, res) {
-  if (err) { return console.error(err); }
-  else {
-    console.log(chalk.green('Your site is connected to SalesForce.com with username: ') + process.env.SF_USERNAME);
-    console.log(chalk.green('Listening on Port: ') + process.env.PORT);
+conn.login(
+  process.env.SF_USERNAME,
+  process.env.SF_PASSWORD + process.env.SF_SECURITY_TOKEN,
+  function(err, res) {
+    if (err) {
+      return console.error(err);
+    } else {
+      console.log(
+        chalk.green(
+          "Your site is connected to SalesForce.com with username: "
+        ) + process.env.SF_USERNAME
+      );
+      console.log(chalk.green("Listening on Port: ") + process.env.PORT);
+    }
   }
-})
+);
 
 //Heroku will put your app to sleep if it does not get traffic for a certain period of time
 // this is a simple script to ping the site once per min to keep it alive with artificial traffic.
 setInterval(function() {
-    http.get("http://apps.shingo.org/");
-}, 60000); // every 5 minutes (300000)
-
+  http.get("http://apps.shingo.org/");
+}, 30000); // every 5 minutes (300000)
 
 //--------------------------------------------------------------------------------------------------------------------------------------
 // GET Home Page
-router.get('/', function(request, response, next) {
+router.get("/", function(request, response, next) {
+  const routes = [
+    "/",
+    "/about/academy",
+    "about/alumni",
+    "about/site-examiners",
+    "about/research-examiners",
+    "about/staff",
+    "about/seab",
+    "about/faculty-fellows",
+    "/affiliates",
+    "awards/prize-recipients",
+    "awards/research",
+    "awards/publication",
+    "/events",
+    "/presentations",
+    "/workshops"
+  ];
 
-    const routes = ['/','/about/academy','about/alumni','about/site-examiners','about/research-examiners','about/staff','about/seab','about/faculty-fellows','/affiliates','awards/prize-recipients','awards/research','awards/publication','/events','/presentations','/workshops'];
-    
-    // RENDER VIEW
-    response.render('index', 
-    { title: 'Shingo Website WC - Home',
-      routes: routes});
+  // RENDER VIEW
+  response.render("index", {
+    title: "Shingo Website WC - Home",
+    routes: routes
+  });
 });
 
 //--------------------------------------------------------------------------------------------------------------------------------------
 // PARTIALS
-// GET Workshop Buttons 
-router.get('/workshop-buttons', function(request, response, next) {
-  response.render('Partials/workshopButtons', 
-    { title: 'Workshop Buttons'
-  });  
+// GET Workshop Buttons
+router.get("/workshop-buttons", function(request, response, next) {
+  response.render("Partials/workshopButtons", { title: "Workshop Buttons" });
 });
 
 module.exports = router;
