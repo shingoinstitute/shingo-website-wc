4388c3af1a43744c219c42983d68b43c454788a8
Justin Bresee <justinbresee@gmail.com>
2020-02-27 12:09:49 -0700

    creating a test REST api for shingo events

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var affiliateRouter = require('./routes/affiliates');
-var workshopRouter = require('./routes/workshops');
-var eventsRouter = require('./routes/events');
-var awardsRouter = require('./routes/awards');
-var aboutRouter = require('./routes/about');
-var presentationRouter = require('./routes/presentations');
+var indexRouter = require("./routes/index");
+var affiliateRouter = require("./routes/affiliates");
+var workshopRouter = require("./routes/workshops");
+var eventsRouter = require("./routes/events");
+var awardsRouter = require("./routes/awards");
+var aboutRouter = require("./routes/about");
+var presentationRouter = require("./routes/presentations");
+var apiRouter = require("./routes/api");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/affiliates', affiliateRouter);
-app.use('/workshops', workshopRouter);
-app.use('/events', eventsRouter);
-app.use('/awards', awardsRouter);
-app.use('/about', aboutRouter);
-app.use('/presentations', presentationRouter);
+app.use("/", indexRouter);
+app.use("/affiliates", affiliateRouter);
+app.use("/workshops", workshopRouter);
+app.use("/events", eventsRouter);
+app.use("/awards", awardsRouter);
+app.use("/about", aboutRouter);
+app.use("/presentations", presentationRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,11 +43,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
