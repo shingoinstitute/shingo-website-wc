9045d5f4e25be2b08ee982d4201640a5c836a061
Justin Bresee <justinbresee@gmail.com>
2020-04-13 16:20:58 -0600

    needed to make edit to workshop query so that it would only pull workshops with the start date being later than the current date

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,7 @@ const conn = new jsforce.Connection();
 conn.login(
   process.env.SF_USERNAME,
   process.env.SF_PASSWORD + process.env.SF_SECURITY_TOKEN,
-  function(err, res) {
+  function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -19,11 +19,11 @@ conn.login(
 );
 
 /* GET Shingo Academy Members Page. */
-router.get("/academy", function(request, response, next) {
+router.get("/academy", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Title, Account.Name FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES('Shingo Academy') ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -34,11 +34,11 @@ router.get("/academy", function(request, response, next) {
 });
 
 /* GET Shingo Alumni Members Page. */
-router.get("/alumni", function(request, response, next) {
+router.get("/alumni", function (request, response, next) {
   //Query SalesForce
   const query =
     "Select Id, Name, Account.Name, Title From Contact Where Shingo_Prize_Relationship__c Includes ('Alumni') ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -49,11 +49,11 @@ router.get("/alumni", function(request, response, next) {
 });
 
 /* GET Shingo Site Examiners Page. */
-router.get("/site-examiners", function(request, response, next) {
+router.get("/site-examiners", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Title, Account.Name FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES('Site Examiner') AND Account.Name != null ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -64,11 +64,11 @@ router.get("/site-examiners", function(request, response, next) {
 });
 
 /* GET Shingo Reseach & Publication Examiners Page. */
-router.get("/research-examiners", function(request, response, next) {
+router.get("/research-examiners", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Title, Account.Name FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES('Research Examiner') ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -82,18 +82,18 @@ router.get("/research-examiners", function(request, response, next) {
       // SEND JSON
       response.send({
         research_examiners: res.records,
-        publication_examiners: res1.records
+        publication_examiners: res1.records,
       });
     });
   });
 });
 
 /* GET Shingo Staff Page. */
-router.get("/staff", function(request, response, next) {
+router.get("/staff", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Name, Title, Email, Phone, Photograph__c,Account.Name  FROM Contact WHERE Account.Name = 'Shingo Institute' ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -104,11 +104,11 @@ router.get("/staff", function(request, response, next) {
 });
 
 /* GET Shingo Executive Advisory Board Page. */
-router.get("/seab", function(request, response, next) {
+router.get("/seab", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Title, Account.Name, Photograph__c FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES('Board of Governors') ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -119,13 +119,13 @@ router.get("/seab", function(request, response, next) {
 });
 
 /* GET Shingo Profile Page for Shingo Executive Advisory Board Member. */
-router.get("/seab/:id", function(request, response, next) {
+router.get("/seab/:id", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Title, Account.Name, Photograph__c,Biography__c FROM Contact WHERE Id = '" +
     request.params.id +
     "' AND Shingo_Prize_Relationship__c INCLUDES('Board of Governors') ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -136,11 +136,11 @@ router.get("/seab/:id", function(request, response, next) {
 });
 
 /* GET Shingo Faculty Fellows Page. */
-router.get("/faculty-fellows", function(request, response, next) {
+router.get("/faculty-fellows", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Title, Account.Name, Photograph__c, Biography__c FROM Contact WHERE Shingo_Prize_Relationship__c INCLUDES('Faculty Fellow') ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -151,13 +151,13 @@ router.get("/faculty-fellows", function(request, response, next) {
 });
 
 /* GET Shingo profile page for faculty fellow. */
-router.get("/faculty-fellows/:id", function(request, response, next) {
+router.get("/faculty-fellows/:id", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Title, Account.Name, Photograph__c,Biography__c FROM Contact WHERE Id = '" +
     request.params.id +
     "' AND Shingo_Prize_Relationship__c INCLUDES('Faculty Fellow') ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -168,11 +168,11 @@ router.get("/faculty-fellows/:id", function(request, response, next) {
 });
 //AWARDS PAGES -----------------------------------------------------------------------------------------------------------------------
 /* GET Shingo Prize Awards Page */
-router.get("/prize-recipients", function(request, response, next) {
+router.get("/prize-recipients", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Name, Date_Awarded__c, City__c, State__c, Country__c, SV_Status__c, Company_Profile_Link__c, Press_Release_Link__c FROM Assessment__c WHERE Publish_to_Website__c=true and Publish_Time_Requirement_Fulfilled__c=false ORDER BY Date_Awarded__c DESC";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -191,11 +191,11 @@ router.get("/prize-recipients", function(request, response, next) {
 });
 
 /* GET Research Awards Page. */
-router.get("/research", function(request, response, next) {
+router.get("/research", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Public_Author_Name__c, Press_Release_Date__c, Press_Release_Link__c FROM Research_Award__c WHERE Published__c=true ORDER BY Press_Release_Date__c DESC";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -214,11 +214,11 @@ router.get("/research", function(request, response, next) {
 });
 
 /* GET Publication Awards Page. */
-router.get("/publication", function(request, response, next) {
+router.get("/publication", function (request, response, next) {
   //Query SalesForce
   const query =
     "Select Id, Name, Public_Author_Name__c, Press_Release_Date__c FROM Publication_Award__c WHERE Published__c=true ORDER BY Press_Release_Date__c DESC";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -237,7 +237,7 @@ router.get("/publication", function(request, response, next) {
 });
 
 /* GET Specific Publication Award Page. */
-router.get("/publication/:id", function(request, response, next) {
+router.get("/publication/:id", function (request, response, next) {
   //Query SalesForce
   const query =
     "Select Id, Name, Public_Author_Name__c, Press_Release_Date__c,Cover_Image__c, Order_Url__c, Published__c, Embedded_Youtube_Url__c, Press_Release_Copy__c FROM Publication_Award__c WHERE Published__c=true AND Id=" +
@@ -245,7 +245,7 @@ router.get("/publication/:id", function(request, response, next) {
     request.params.id +
     "'" +
     " ORDER BY Press_Release_Date__c DESC";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -258,11 +258,11 @@ router.get("/publication/:id", function(request, response, next) {
 // AFFILIATES---------------------------------------------------------------------------------------------------
 
 /* GET Affiliates Page. */
-router.get("/affiliates", function(request, response, next) {
+router.get("/affiliates", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Logo__c, Page_Path__c, Website, Languages__c FROM Account WHERE RecordType.Name='Licensed Affiliate' AND (NOT Name LIKE 'McKinsey%') AND (NOT Name LIKE 'Shingo Institute%') AND (NOT Name LIKE 'MyEducator%') ORDER BY Name ASC";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -272,13 +272,13 @@ router.get("/affiliates", function(request, response, next) {
 });
 
 /* GET Affiliate page for a specific affiliate. */
-router.get("/affiliates/:id", function(request, response, next) {
+router.get("/affiliates/:id", function (request, response, next) {
   //Query SalesForce (first for faciliators related to affiliate)
   var query =
     "SELECT Id, Name, Title, Biography__c, Photograph__c, Account.Name FROM Contact WHERE Facilitator_For__r.Id='" +
     request.params.id +
     "' ORDER BY LastName";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -289,7 +289,7 @@ router.get("/affiliates/:id", function(request, response, next) {
       "'" +
       request.params.id +
       "'";
-    conn.query(query, function(err, res1) {
+    conn.query(query, function (err, res1) {
       if (err) {
         return console.error(err);
       }
@@ -297,7 +297,7 @@ router.get("/affiliates/:id", function(request, response, next) {
       // SEND JSON
       response.send({
         affiliate: res1.records[0],
-        facilitators: res.records
+        facilitators: res.records,
       });
     });
   });
@@ -306,11 +306,11 @@ router.get("/affiliates/:id", function(request, response, next) {
 // EVENTS---------------------------------------------------------------------------------------------------
 
 /* GET Current Shingo Events. */
-router.get("/events", function(request, response, next) {
+router.get("/events", function (request, response, next) {
   //Query SalesForce
   const query =
     "SELECT Id, Name, Event_Type__c,Start_Date__c,End_Date__c,Registration_Link__c, Display_Location__c, Banner_URL__c  FROM Shingo_Event__c WHERE Start_Date__c > TODAY and Publish_to_Web_App__c = true and Content_on_CVENT__c = true ORDER BY Start_Date__c";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -338,11 +338,11 @@ router.get("/events", function(request, response, next) {
 // WORKSHOPS---------------------------------------------------------------------------------------------------
 //WORKSHOP PAGES
 /* GET Workshop Schedule Page */
-router.get("/workshops", function(request, response, next) {
+router.get("/workshops", function (request, response, next) {
   //Query SalesForce
   const query =
-    "SELECT Id,Name,Workshop_Type__c,Start_Date__c, End_Date__c,Timezone__c,Event_City__c,Event_Country__c, Host_Site__c,Affiliate__c,Registration_Website__c,RecordType.Name FROM Workshop__c WHERE Public__c=true AND (Status__c='Verified' OR Status__c='Action Pending') ORDER BY Start_Date__c";
-  conn.query(query, function(err, res) {
+    "SELECT Id,Name,Workshop_Type__c,Start_Date__c, End_Date__c,Timezone__c,Event_City__c,Event_Country__c, Host_Site__c,Affiliate__c,Registration_Website__c,RecordType.Name FROM Workshop__c WHERE Public__c=true AND (Status__c='Verified' OR Status__c='Action Pending') AND Start_Date__c > TODAY ORDER BY Start_Date__c";
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
@@ -386,22 +386,22 @@ router.get("/workshops", function(request, response, next) {
       title: "Shingo Workshop Schedule",
       workshops: res.records,
       workshopTypes: workshopTypes,
-      workshopCountries: workshopCountries
+      workshopCountries: workshopCountries,
     });
   });
 });
 
 /* GET Specific Workshop Page. */
 // The workshop type is specified in the URL and then passed into the query
-router.get("/:workshopType", function(request, response, next) {
+router.get("/:workshopType", function (request, response, next) {
   //Query SalesForce
   const query =
-    "SELECT Id,Name,Workshop_Type__c,Start_Date__c, End_Date__c, Timezone__c, Event_City__c,Event_Country__c, Host_Site__c,Affiliate__c,Registration_Website__c,RecordType.Name FROM Workshop__c WHERE Public__c=true AND (Status__c='Verified' OR Status__c='Action Pending') and Workshop_Type__c = " +
+    "SELECT Id,Name,Workshop_Type__c,Start_Date__c, End_Date__c, Timezone__c, Event_City__c,Event_Country__c, Host_Site__c,Affiliate__c,Registration_Website__c,RecordType.Name FROM Workshop__c WHERE Public__c=true AND (Status__c='Verified' OR Status__c='Action Pending')AND Start_Date__c > TODAY AND Workshop_Type__c = " +
     "'" +
     request.params.workshopType +
     "'" +
     " ORDER BY Start_Date__c";
-  conn.query(query, function(err, res) {
+  conn.query(query, function (err, res) {
     if (err) {
       return console.error(err);
     }
